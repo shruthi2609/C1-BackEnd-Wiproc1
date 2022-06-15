3bb19bb6c583166bdc22c8681bf4d8fe1bae417e
shruthi093 <jayasruthi.l@gmail.com>
2022-06-15 18:00:56 +0530

    15-06 express

---
+++ expressdemoc1/index.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const server=express()
+
+server.get("/home",(req,res)=>{
+console.log(req)
+res.status(200).send("hello express js")
+})
+
+server.get("/",(req,res)=>{
+    console.log(req)
+    res.status(200).send("home page")
+})
+
+server.get("/contact",(req,res)=>{
+    const userdata={
+        fname:"john",
+        email:"john@gmail.com"
+    }
+    res.send(JSON.stringify(userdata))
+
+})
+
+server.all("*",(req,res)=>{
+    res.status(400).send("page not found")
+})
+
+const app=server.listen(3001,()=>{
+  // console.log(app.address().address)
+    console.log(`server started at ${app.address().port}`)
+})
