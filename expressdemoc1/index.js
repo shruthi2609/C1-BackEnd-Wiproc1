0b401d231f9cb1251c14b4187fabb8cc037f790d
shruthi093 <jayasruthi.l@gmail.com>
2022-06-16 18:03:28 +0530

    16.06

--- expressdemoc1/index.js
+++ expressdemoc1/index.js
@@ -1,6 +1,10 @@
 const express=require("express")
 const server=express()
-
+const bp=require("body-parser")
+const jsonparser=bp.json() //apply on second arg of any post or put or patch method
+server.use(bp.json()) // apply on all the post req-  use  method
+server.use(bp.text())
+//req.url===="/home" req.method===get
 server.get("/home",(req,res)=>{
 console.log(req)
 res.status(200).send("hello express js")
@@ -16,10 +20,24 @@ server.get("/contact",(req,res)=>{
         fname:"john",
         email:"john@gmail.com"
     }
-   
-    res.send(JSON.stringify(userdata))
-
+    //res.status(200).send(JSON.stringify(userdata))
+    res.writeHead(200,{'Content-Type':'application/json'})
+    res.end(JSON.stringify(userdata))
 })
+
+
+//post request
+server.post("/contact",(req,res)=>{
+    const reqdata=req.body
+    console.log(reqdata)
+    res.send("set contact")
+})
+
+server.post("/setdetails",(req,res)=>{
+    console.log(req.body)
+    res.send("set details")
+})
+
 
 server.all("*",(req,res)=>{
     res.status(400).send("page not found")
