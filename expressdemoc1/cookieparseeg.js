6e73ca923a40ae6e3153d99b6541dbd937e44beb
shruthi093 <jayasruthi.l@gmail.com>
2022-06-22 17:59:21 +0530

    22-06

---
+++ expressdemoc1/cookieparseeg.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const server=express()
+const cookieParser=require("cookie-parser")
+server.use(cookieParser())
+const bp=require("body-parser")
+const jsonparser=bp.json()
+server.use(bp.json())
+
+server.post("/signup",(req,res)=>{
+    const value=req.body.uname
+    res.cookie('userdetails','someadded')
+    res.send("signup successfully")
+})
+
+server.get("/",(req,res)=>{
+    console.log(req.cookies['userdetails'])
+    res.send("home page")
+})
+
+server.listen(3001,()=>console.log("started"))
