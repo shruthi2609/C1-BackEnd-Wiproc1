4c9509ee6bc9cedc83fef9e1b1b9fc3899ea0521
shruthi093 <jayasruthi.l@gmail.com>
2022-07-07 18:00:14 +0530

    Bcrypt compare and validations

--- sinontesting/models/UsersModel.js
+++ sinontesting/models/UsersModel.js
@@ -4,12 +4,17 @@ const Schema=mongoose.Schema
 const UserSchema=new Schema({
     email:{
         type:String,
-        required:true,
+        required:[true,"email is empty please enter email"],
         unique:true
     },
     password:{
         type:String,
         required:true
+       
+    },
+    role:{
+      type:String,
+      enum:["admin","trainer","trainee"]
     }
 })
 const UserModel=mongoose.model("UserModel",UserSchema)
--- sinontesting/routes/AuthenticationRoutes.js
+++ sinontesting/routes/AuthenticationRoutes.js
@@ -4,10 +4,18 @@ const bcrypt=require("bcrypt")
 const UserModel=require("../models/UsersModel")
 router.post("/signup",async (req,res)=>{
     const data=req.body
+    if(data.role==="admin"||data.roles==="trainer"||data.roles==="trainee")
+    {
   /*  const salt=await bcrypt.genSalt(4)
     console.log(salt)
     const hashedpassword=await bcrypt.hash(data.password,salt)
     console.log(hashedpassword)*/
+    const user=await UserModel.findOne({email:data.email}
+    )
+    if(user){
+      res.status(400).send({msg:"email exists already",status:false})
+    }
+    else{
     const hashedpassword=await bcrypt.hash(data.password,4)
     console.log(hashedpassword)
     try{
@@ -20,6 +28,32 @@ router.post("/signup",async (req,res)=>{
     catch(err){
         res.status(404).send(err)
     }
+  }
+}
+else{
+  res.send("role is not supported")
+}
+})
 
+router.post("/signin",async (req,res)=>{
+  const data=req.body
+  try{
+  const user=await UserModel.findOne({email:data.email})
+  if(user){
+  const comparison=await bcrypt.compare(data.password,user.password)
+  if(comparison){
+    res.status(200).send({msg:"login successfull",status:true})
+  }
+  else{
+    res.status(404).send({msg:"login is not successfull , please check your password",status:false})
+  }
+  }
+  else{
+    res.status(404).send({msg:"email does not exists, please check your email",status:false})
+  }
+  }
+  catch(err){
+    res.status(404).send(err)
+  } 
 })
 module.exports=router
